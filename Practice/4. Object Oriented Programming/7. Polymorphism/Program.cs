2ee58b2c7080784a29efcde983363ef44e6680de
Jay Makhija <jay13makhija@gmail.com>
2022-08-11 23:29:47 +0530

    poly and encap

---
+++ Practice/4. Object Oriented Programming/7. Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Poly
+{
+    public virtual void Sample()
+    {
+        Console.WriteLine("Virtual function");
+    }
+}
+
+public class Poly2: Poly
+{
+    public override void Sample()
+    {
+        Console.WriteLine("Override Done!!");
+    }
+}
+
+public class Poly3: Poly2
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public float Add(float a, float b)
+    {
+        return a + b;
+    }
+}
+
+public class Poly4: Poly3
+{
+    
+    public static void Main()
+    {
+        Poly4 p1 = new Poly4();
+        // This is example of RunTime Polymorphism (Dynamic) (Early-binding) (Overriding)
+        p1.Sample();
+        // This is example of CompileTime Polymorphism (Static) (Late-binding) (Overloading)
+        Console.WriteLine(p1.Add(2, 5));
+        Console.WriteLine(p1.Add(2.3f, 4.5f));
+
+    }
+}
---
+++ Practice/4. Object Oriented Programming/8. Encapsulation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Informataion
+{
+    private int id;   //field
+    public int Id     // property
+    {
+        get { return id; } 
+        set { id = value; }
+    }
+    private string name;  // field
+    public string Name    // property
+    { 
+        get { return name; }
+        set { name = value; }     
+    }
+    private string company;   // field
+    public string Company     // property
+    { 
+        get { return company; } 
+        set { company = value; } 
+    }
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        
+
+        int num;
+        Console.WriteLine("Enter number of People");
+        num = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < num; i++)
+        {
+            Informataion info = new Informataion();
+            
+            Console.WriteLine("Enter Id");
+            info.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name");
+            info.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Company");
+            info.Company = Console.ReadLine();
+
+            Console.WriteLine("Name entered is " + info.Name + "\n");
+            Console.WriteLine("Id entered is  " + info.Id + "\n");
+            Console.WriteLine("Company entered is " + info.Company + "\n");
+
+        }
+        
+
+    }
+}
