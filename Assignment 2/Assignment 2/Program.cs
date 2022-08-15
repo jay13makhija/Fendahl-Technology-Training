bd3ab3eaa4e3cd00cebeaa31a5cf74d4151de5ac
Jay Makhija <jay13makhija@gmail.com>
2022-08-16 00:36:56 +0530

    assignment update

---
+++ Assignment 2/Assignment 2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*                                         Assignment 2:
+
+Consider Class Demo1 with instance methods D1() and D2().
+
+The class Demo1 has only 1 user-defined constructor and it is marked as private. 
+
+Considering the above situation, we can not instantiate the class Demo1 in any other 
+class and also can not be able to access its method. 
+
+You are allowed to modify the class but do not add a constructor, do not inherit the
+class Demo1 &also do not modify the existing constructor.
+
+Try to find the solution to access the instance methods D1 and D2.*/
+
+using System;
+
+public class Demo1
+{
+    public static void D1()
+    {
+        Console.WriteLine("This is D1");
+    }
+    public static void D2()
+    {
+        Console.WriteLine("This is D2");
+    }
+
+    private Demo1()
+    {
+        Console.WriteLine("This is constructor");
+    }
+}
+
+public class Main_Class
+{
+    public static void Main()
+    {
+        Demo1.D1();
+        Demo1.D2();
+    }
+}
---
+++ Assignment 3/Assignment 3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*                                      Assignment 3
+ 
+Consider a base class MyClassA { }
+with few instance methods inside & a derived class of MyClassA is class MyClassB { } 
+
+Class MyClassA { }
+has only 1 constructor which is parameterized & MyClassB has no user defined constructor. 
+
+Are we able to create an instance of the class MyClassB with following statement in main method 
+
+MyClassB mb1 = new MyClassB();
+
+if yes / no then why ? */
+
+//  Answer is NO
+
+using System;
+public class MyClassA
+{
+    private void Sample()
+    {
+        Console.WriteLine("Inside Sample");
+    }
+    public MyClassA(int num)
+    {
+        Console.WriteLine(num);
+    }
+    /*public MyClassA()                               // Solution 1 to the problem
+    {
+
+    }*/
+}
+
+public class MyClassB : MyClassA
+{
+    /*public MyClassB(int num) : base(num)
+    {                                                 // Solution 2 of the problem
+
+    }*/
+    public static void Main()
+    {
+        /*int num = 10;*/
+        MyClassB mb1 = new MyClassB(/*num*/);
+    }
+}
